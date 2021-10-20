86b8da2095b95559783502e869b0b8aaec2dd778
leroyabiguime <mataa.abiguime17@gmail.com>
2021-10-20 15:23:28 +0000

    Add Task part done with array ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import PropTypes  from "prop-types"
 import Button from "./Button"
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("click")
-    }
+const Header = ({ title, onAdd }) => {
+  
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button onClick={onClick} color='green' text='Add' />
+            <Button onClick={onAdd} color='green' text='Add' />
         </header>
     )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,37 +1,13 @@
-import React from 'react'
+import Task from "./Task"
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Food Shoping',
-        day: 'Feb 7th at 1:30pm',
-        reminder: false,
-    },
-    {
-        id: 4,
-        text: 'Client Meeting',
-        day: 'Feb 8th at 8:30am',
-        reminder: true,
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({tasks, onDelete, onToogle}) => {
+   
     return (
-        <div>
-            
-        </div>
+        <>
+        {tasks.map((task) => (
+            <Task key={task.id} task={task}
+            onDelete={onDelete} onToogle={onToogle}></Task>))}
+        </>
     )
 }
 
